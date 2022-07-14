2dbf330814d33799d7ac6c39dca14ddf5043b5b2
xuanloi99 <20211TT0219@mail.tdc.edu.vn>
2022-07-14 23:01:21 +0700

    Set up enviroment ReactJS

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+function App() {
+    return (
+        <div>
+            <h1>Xin chao F8</h1>
+            <h1>Bye</h1>
+            <h1>Thuong lam moi sua</h1>
+        </div>
+    )
+}
+ReactDOM.render(<App />, document.getElementById("root"))
